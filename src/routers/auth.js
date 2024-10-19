17e85fdf985e654763df3507dad3cc9563d5703f
YevheniiKuzmishen <yevhenii.kuzmishen@gmail.com>
2024-10-19 18:10:18 +0200

    add register-login-refresh-logout-resetPassword-authenticate-cloudinary

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,49 @@
 import { Router } from 'express';
-import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { validateBody } from '../utils/validateBody.js';
 
-export const authRouetr = Router();
+import ctrlWrapper from '../utils/ctrlWrapper.js';
+import validateBody from '../utils/validateBody.js';
+
+import {
+  userLoginSchema,
+  userRegisterSchema,
+  sendResetEmailSchema,
+  resetPasswordSchema,
+} from '../validation/users.js';
+import {
+  registerController,
+  loginController,
+  refreshController,
+  logoutController,
+  sendResetEmailController,
+  resetPasswordController,
+} from '../controllers/auth.js';
+
+export const authRouter = Router();
+
+authRouter.post(
+  '/register',
+  validateBody(userRegisterSchema),
+  ctrlWrapper(registerController),
+);
+
+authRouter.post(
+  '/login',
+  validateBody(userLoginSchema),
+  ctrlWrapper(loginController),
+);
+
+authRouter.post('/refresh', ctrlWrapper(refreshController));
+
+authRouter.post('/logout', ctrlWrapper(logoutController));
+
+authRouter.post(
+  '/send-reset-email',
+  validateBody(sendResetEmailSchema),
+  ctrlWrapper(sendResetEmailController),
+);
+
+authRouter.post(
+  '/reset-pwd',
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController),
+);
