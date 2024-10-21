983f5e416147270e302e5067c8fbd2bb6bc7da8a
PavloHohish <pasha.steam@ukr.net>
2024-10-21 19:42:06 +0300

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -11,11 +11,15 @@ const setupSession = (res, session) => {
   res.cookie('refreshToken', session.refreshToken, {
     httpOnly: true,
     expire: new Date(Date.now() + session.refreshTokenValidUntil),
+    sameTime: 'None',
+    secure: true,
   });
 
   res.cookie('sessionId', session._id, {
     httpOnly: true,
     expire: new Date(Date.now() + session.refreshTokenValidUntil),
+    sameTime: 'None',
+    secure: true,
   });
 };
 
