43096bea6e5ea9fbf6f4bd627b34d1df5e0e91cf
PavloHohish <pasha.steam@ukr.net>
2024-10-23 11:55:24 +0300

    update/swagger

--- src/validation/users.js
+++ src/validation/users.js
@@ -26,5 +26,5 @@ export const patchUserSchema = Joi.object({
   username: Joi.string(),
   email: Joi.string().pattern(emailRegexp),
   gender: Joi.string().valid('woman', 'man'),
-  dailynormwater: Joi.number().min(0),
+  dailynormwater: Joi.number().min(0).max(15000),
 });
