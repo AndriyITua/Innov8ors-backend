cc01c7bed269f55236efbf303899d5e9b06c14ad
YevheniiKuzmishen <yevhenii.kuzmishen@gmail.com>
2024-10-25 14:57:15 +0200

    fix login

--- src/middlewares/handleUniqueError.js
+++ src/middlewares/handleUniqueError.js
@@ -2,7 +2,7 @@ import createHttpError from 'http-errors';
 
 const handleUniqueError = (err, req, res, next) => {
   if (err.code === 11000) {
-    return next(createHttpError(400, 'Email already exists'));
+    return next(createHttpError(409, 'Email already exists'));
   }
 
   next(err);
